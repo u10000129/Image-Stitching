e1eba2c9892805d3990df9bd4a834419a44751cd
Tao <zxc5540599@gmail.com>
2017-12-24 22:10:57 +0800

    Update utils.py

--- code/utils.py
+++ code/utils.py
@@ -8,6 +8,11 @@ def im2float(im):
     if im.dtype == np.uint8:
         return im / 255.0
 
+def float2im(im):
+    if im.dtype == np.float32 or im.dtype == np.float64:
+        return (im * 255.0).astype(np.uint8)
+
+
 def normalize(im):
     _im = im - np.min(im)
     return _im / np.max(_im)
